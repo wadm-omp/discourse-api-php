427a2db59159c63ffcde1a59005fa57e5558bc27
Eric Mueller <eric@ericmueller.org>
2020-04-01 17:30:21 +0200

    improved tests

--- tests/DiscourseApiTest.php
+++ tests/DiscourseApiTest.php
@@ -11,6 +11,10 @@ class DiscourseApiTest extends TestCase {
 
 	private $testUserName;
 
+	private $testCategory;
+	private $testTopic;
+
+
 	protected function setUp() {
 		// load environment vars from .env file
 		// see https://github.com/vlucas/phpdotenv
@@ -28,7 +32,7 @@ class DiscourseApiTest extends TestCase {
 	/**
 	 * test the getCategories() call. assumes your Discourse installation has at least one category!
 	 *
-	 * @group common
+	 * @group topics
 	 */
 	public function testGetCategories() {
 		$res = $this->DiscourseAPI->getCategories();
@@ -38,7 +42,7 @@ class DiscourseApiTest extends TestCase {
 		$this->assertIsObject( $res->apiresult );
 
 		// then let's be sure we got a valid result
-		$this->assertEquals( '200', $res->http_code );
+		$this->assertEquals( 200, $res->http_code );
 
 		// then let's be sure there is at least one category
 		$this->assertGreaterThan( 0, sizeof( $res->apiresult->category_list->categories ),
@@ -46,17 +50,19 @@ class DiscourseApiTest extends TestCase {
 
 	}
 
-
 	/**
 	 * test the getCategory() call. assumes your Discourse installation has at least one category!
 	 * this call is to get full info on a single category
 	 *
 	 * @depends testGetCategories
-	 * @group common
+	 *
+	 * @group   topics
 	 */
 	public function testGetCategory() {
 		// first get all categories
 		$res = $this->DiscourseAPI->getCategories();
+
+		$this->assertEquals( 200, $res->http_code );
 
 		// pull out the first category
 		$firstCategory = $res->apiresult->category_list->categories[0];
@@ -64,12 +70,17 @@ class DiscourseApiTest extends TestCase {
 		// use ID to get full info (this is the method we're testing)
 		$catInfo = $this->DiscourseAPI->getCategory( $firstCategory->id );
 
+		$this->assertEquals( 200, $catInfo->http_code );
+
+		//var_dump($catInfo->apiresult );die();
 		$this->assertIsObject( $catInfo->apiresult );
-		$this->assertIsArray( $catInfo->apiresult->users, "Can't retrieve array of users from getCategory()" );
-		$this->assertIsArray( $catInfo->apiresult->topic_list->topics, "Can't retrieve array of topics from getCategory()" );
+		$this->assertIsArray( $catInfo->apiresult->users, "Can't retrieve array of users from getCategory() " . $firstCategory->id );
+		$this->assertIsArray( $catInfo->apiresult->topic_list->topics, "Can't retrieve array of topics from getCategory() " . $firstCategory->id );
 
 		// use SLUG to get full info (this is the method we're testing)
 		$catInfo = $this->DiscourseAPI->getCategory( $firstCategory->slug );
+
+		$this->assertEquals( 200, $catInfo->http_code );
 
 		$this->assertIsObject( $catInfo->apiresult );
 		$this->assertIsArray( $catInfo->apiresult->users, "Can't retrieve array of users from getCategory()" );
@@ -81,7 +92,8 @@ class DiscourseApiTest extends TestCase {
 	 * test the getTopic() call. assumes your Discourse installation has at least one category!
 	 * this call is to get full info on a single category
 	 *
-	 * @group common
+	 * @group   topics
+	 *
 	 * @depends testGetCategories
 	 * @depends testGetCategory
 	 */
@@ -101,31 +113,114 @@ class DiscourseApiTest extends TestCase {
 		// here is the method we're testing
 		$topicInfo = $this->DiscourseAPI->getTopic( $firstTopic->id );
 
+		$this->assertEquals( 200, $topicInfo->http_code );
+
 		$this->assertEquals( $topicInfo->apiresult->id, $firstTopic->id );
 		$this->assertEquals( $topicInfo->apiresult->name, $firstTopic->name );
 	}
 
 	/**
+	 * @group   topics
+	 *
+	 * @depends testGetCategories
+	 * @depends testGetCategory
+	 * @depends testGetTopic
+	 */
+
+	public function testCreateTopic() {
+		// first get all categories
+		$res = $this->DiscourseAPI->getCategories();
+
+		// pull out the first category
+		$firstCategory = $res->apiresult->category_list->categories[0];
+
+		$topicName = "Here is my test topic that has a long title and random number, " . mt_rand( 1, 999 );
+
+		$bodyText = $this->getDummyPargraphs( 3 );
+		$res      = $this->DiscourseAPI->createTopic( $topicName, $bodyText, $firstCategory->id, 'system' );
+
+		$this->assertEquals( 200, $res->http_code );
+		$this->assertEquals( 'system', $res->apiresult->username );
+		$this->assertEquals( 1, $res->apiresult->post_number );
+		$this->assertEquals( 0, $res->apiresult->reply_count );
+	}
+
+	/**
+	 * @group   topics
+	 *
+	 * @depends testGetCategories
+	 * @depends testGetCategory
+	 * @depends testGetTopic
+	 * @depends testCreateTopic
+	 */
+	public function testCreatePost() {
+		// first get all categories
+		$res = $this->DiscourseAPI->getCategories();
+
+		// pull out the first category
+		$firstCategory = $res->apiresult->category_list->categories[0];
+
+		$topicName = "Here is my test topic that has a long title and random number, " . mt_rand( 1, 999 );
+
+		$bodyText  = $this->getDummyPargraphs( 3 );
+		$bodyText2 = $this->getDummyPargraphs( 3 );
+
+		$topicResult = $this->DiscourseAPI->createTopic( $topicName, $bodyText, $firstCategory->id, 'system' );
+
+		$this->assertIsInt( $topicResult->apiresult->topic_id );
+		$this->assertNotEquals( 0, $topicResult->apiresult->id, 'Topic ID of new topic is 0' );
+
+		$res = $this->DiscourseAPI->createPost( $bodyText2, $topicResult->apiresult->topic_id, 'system' );
+	}
+
+
+	/**
+	 * get dummy text from randomtext.me service
+	 *
+	 * @param $grafCount
+	 *
+	 * @return string
+	 */
+	private function getDummyPargraphs( $grafCount ) {
+		$url = 'https://www.randomtext.me/api/gibberish/p-' . $grafCount . '/2-10';
+
+		do {
+			$jsonData = @file_get_contents( $url );
+
+			if ( ! $jsonData ) {
+				// if we didn't get anything, sleep for 1 sec & try again
+				sleep( 1 );
+			}
+		} while ( ! $jsonData );
+
+		$obj = json_decode( $jsonData );
+
+		$dummyText = (string) $obj->text_out;
+
+		// change into plaintext
+		$dummyText = str_replace( [ '<p>', "\r" ], "", $dummyText );
+		$dummyText = str_replace( '</p>', "\n\n", $dummyText );
+
+		return trim( $dummyText );
+	}
+
+
+	/**
+	 *
+	 * @group users
 	 */
 	public function testCreateUser() {
+		$userName     = $realName = 'erictest' . mt_rand( 10, 999 );
+		$emailAddress = 'eric+' . $userName . '@ericmueller.org';
+		$password     = 'password' . mt_rand( 1000, 9999 );
 
-		$testUserName = 'dummyaccount' . mt_rand( 0, 999 );
+		$res = $this->DiscourseAPI->createUser( $realName, $userName, $emailAddress, $password );
 
-		$params = [
-			'location' => 'Home!',
-		];
+		$this->assertGreaterThan( 0, $res->apiresult->user_id, 'User ID is 0' );
+		$this->assertEquals( true, $res->apiresult->success, 'createUser did not return success' );
 
-		$realName     = 'erictest5';
-		$userName     = 'erictest5';
-		$emailAddress = 'eric+erictest5@ericmueller.org';
-		$password     = 'password';
-
-		$this->DiscourseAPI->createUser( $realName, $userName, $emailAddress, $password );
-
-
-		//			$res = $this->DiscourseAPI->setUserInfo( $testUserName, $params );
-
-		//		var_dump( $res );
+		$r = $this->DiscourseAPI->updateUser( $userName, [ 'location' => 'At home right now' ] );
+		var_dump( $r );
 	}
 
 	/**
