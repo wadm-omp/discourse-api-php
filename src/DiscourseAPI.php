4f773fc673eb0a2fa009c5737d26508f82be26fb
Eric Mueller <eric@ericmueller.org>
2020-04-14 21:59:40 +0200

    added setDebugGetRequest() and setUserField() methods

--- src/DiscourseAPI.php
+++ src/DiscourseAPI.php
@@ -359,7 +359,6 @@ class DiscourseAPI {
 			}
 		}
 
-		# This must PUT
 		return $this->_putRequest( '/categories/' . $catid, [ $params ] );
 	}
 
@@ -1168,11 +1167,22 @@ class DiscourseAPI {
 	}
 
 	/**
+	 * @param bool $debugGetRequest
 	 */
+	public function setDebugGetRequest( bool $debugGetRequest ): void {
+		$this->debugGetRequest = $debugGetRequest;
 	}
 
+
 	/**
+	 * @param $username
+	 * @param $params
+	 *
+	 * @return stdClass
+	 * @throws Exception
 	 */
+	public function setUserField( $username, $params ) {
+		return $this->_putRequest( '/users/' . $username, [ $params ] );
 	}
 
 }
