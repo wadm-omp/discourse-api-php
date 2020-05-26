fe18e79a86cd66165d95c0ae04a9566e8caa42ab
Eric Mueller <eric@ericmueller.org>
2020-05-26 14:15:29 +0200

    improving example code

--- example.php
+++ example.php
@@ -1,12 +1,13 @@
 <?php
 
-use pnoeric\discourseAPI\DiscourseAPI;
+use pnoeric\DiscourseAPI;
 
 // ideally you have a composer auto-loader in place, but if not, you can just require the library:
 // require_once 'lib/DiscourseAPI.php';
 
+// note that you need to specify the URL of your Discourse install, and
 // for API_KEY you'll need your key from Discourse
-$api = new DiscourseAPI( 'apitest.discoursehosting.net', API_KEY, 'https' );
+$api = new DiscourseAPI( DISCOURSE_URL, API_KEY, 'https' );
 
 // create user
 $r = $api->createUser( 'John Doe', 'johndoe', 'johndoe@discoursehosting.com', 'foobar!!' );
@@ -29,7 +30,7 @@ $catId = $r->apiresult->category->id;
 // create a topic in a category
 $r = $api->createTopic(
 	'This is the title of a brand new topic',
-	"This is the body text of a brand new topic. I really don't know what to say",
+	"This is the body text of a brand new topic. What else is there to say? Enjoy the topic! Hurrah!",
 	$catId,
 	'johndoe'
 );
@@ -38,18 +39,21 @@ print_r( $r );
 $topicId = $r->apiresult->id;
 
 // create a post in a topic
+$now = new DateTime();
+
 $r = $api->createPost(
 	'This is the body of a new post in an existing topic',
 	$topicId,
-	'johndoe'
+	'johndoe',
+	$now
 );
 
-// change sitesetting
+// change site setting (these are admin-only settings, NOT user preferences)
 // use 'true' and 'false' between quotes
-
 $r = $api->changeSiteSetting( 'invite_expiry_days', 29 );
 print_r( $r );
 
 // log off user
 $api->logoutUserByUsername( 'johndoe' );
 
+// and there are many, many more methods in the API... check 'em out!
